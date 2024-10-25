2822fbb282f707755a31e287f0de2fabea2f9439
userA <geniusssbg@gmail.com>
2024-10-25 22:32:24 +0800

    application

--- cyber-nomads-app/src/main/java/io/github/geniusay/CyberNomadsApplication.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/CyberNomadsApplication.java
@@ -1,4 +1,13 @@
 package io.github.geniusay;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CyberNomadsApplication {
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(CyberNomadsApplication.class, args);
+    }
 }
