fd3e06d0a885985d0eb9366f3aad9db231db18a7
li <3250162035@qq.com>
2024-10-28 00:40:31 +0800

    代理池 获取策略编写（未初始化代理池）

--- cyber-nomads-app/src/main/java/io/github/geniusay/Mapper/ProxyMapper.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/Mapper/ProxyMapper.java
@@ -1,10 +1,10 @@
-package io.github.geniusay.mapper;
+package io.github.geniusay.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import io.github.geniusay.pojo.Proxy;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public class ProxyMapper extends BaseMapper<Proxy> {
+public interface ProxyMapper extends BaseMapper<Proxy> {
 
 }
