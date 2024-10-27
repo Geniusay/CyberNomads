1d1f36218565eaa43d0645a311cd1edf4873d91f
userA <geniusssbg@gmail.com>
2024-10-27 19:42:11 +0800

    增加邮箱发送验证码功能

---
+++ cyber-nomads-app/src/main/java/io/github/geniusay/utils/RandomUtil.java
@@ -0,0 +1,19 @@
+package io.github.geniusay.utils;
+
+import java.util.Random;
+
+public class RandomUtil {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String generateRandomString(int len) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(len);
+
+        for (int i = 0; i < len; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+
+        return sb.toString();
+    }
+}
