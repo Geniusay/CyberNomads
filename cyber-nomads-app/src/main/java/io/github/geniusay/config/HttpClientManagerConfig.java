6403c02c27db694a68011d0617910e3b2880bc67
li <3250162035@qq.com>
2024-10-28 20:05:10 +0800

    修复bean

--- cyber-nomads-app/src/main/java/io/github/geniusay/config/HttpClientManagerConfig.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/config/HttpClientManagerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -17,7 +18,8 @@ public class HttpClientManagerConfig {
 
     @Bean
     public ProxyManager proxyManager() {
-        List<Proxy> proxies = proxyMapper.selectList(new LambdaQueryChainWrapper<>(proxyMapper));
+//        List<Proxy> proxies = proxyMapper.selectList(new LambdaQueryChainWrapper<>(proxyMapper));
+        List<Proxy> proxies = new ArrayList<>();
         return new ProxyManager(proxies);
     }
 }
