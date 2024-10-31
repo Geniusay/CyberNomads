9c69d76a437b3390c8512d5511f4cb22cc527c1a
li <3250162035@qq.com>
2024-11-01 01:33:17 +0800

    <Auto> 添加caffine解决邮箱发送冷却 -v:1.0.0 -rp:9000 -de:<-e ACTIVE=prod -e SERVER_PORT=9000>

--- cyber-nomads-app/src/main/java/io/github/geniusay/CyberNomadsApplication.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/CyberNomadsApplication.java
@@ -2,8 +2,10 @@ package io.github.geniusay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CyberNomadsApplication {
     public static void main( String[] args )
     {
