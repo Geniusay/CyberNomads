e6899d958624cd2d296d386991990cdf81567d43
li <3250162035@qq.com>
2024-11-10 22:18:21 +0800

    <Auto>  -v:1.0.0 -rp:9000 -de:<-e ACTIVE=prod -e SERVER_PORT=9000>

--- cyber-nomads-app/src/main/java/io/github/geniusay/service/Impl/IUserService.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/service/Impl/IUserService.java
@@ -81,7 +81,7 @@ public class IUserService implements UserService {
     public LoginVO register(RegisterRequestDTO req) {
         String code = req.getCode();
         String cacheCode = cacheUtil.getEmailAndRemove(req.getEmail());
-        if(!code.equals(cacheCode)){
+        if(!code.equals(cacheCode.toLowerCase())){
             throw new ServeException("验证码错误");
         }
         UserDO user = UserDO.builder()
