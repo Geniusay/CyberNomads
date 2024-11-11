c98058cd49785fe571d9bf9a18fe5cb9958ca9ae
li <3250162035@qq.com>
2024-11-11 19:09:51 +0800

    <Auto>  -v:1.0.0 -rp:9000 -de:<-e ACTIVE=prod -e SERVER_PORT=9000>

--- cyber-nomads-app/src/main/java/io/github/geniusay/service/Impl/IFAQService.java
+++ cyber-nomads-app/src/main/java/io/github/geniusay/service/Impl/IFAQService.java
@@ -3,6 +3,7 @@ package io.github.geniusay.service.Impl;
 import io.github.geniusay.mapper.AnswerQuestionMapper;
 import io.github.geniusay.pojo.DO.AnswerQuestion;
 import io.github.geniusay.service.FAQService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+@Slf4j
 public class IFAQService implements FAQService {
 
     @Resource
@@ -23,20 +25,23 @@ public class IFAQService implements FAQService {
     private final Object lock = new Object();
 
     @PostConstruct
-    public void init() {
+    public List<AnswerQuestion> init() {
+        log.info("问题和回答缓存初始化...");
         List<AnswerQuestion> answerQuestions = answerQuestionMapper.selectList(null);
         redisTemplate.opsForList().leftPushAll("answerQuestions", answerQuestions);
+        log.info("问题和回答缓存初始化成功，当前缓存数量{}", answerQuestions.size());
+        return answerQuestions;
     }
 
 
     @Override
     public List<AnswerQuestion> getFAQ() {
         List<AnswerQuestion> list = redisTemplate.opsForList().range("answerQuestions", 0, -1);
-        if (list != null && list.isEmpty()){
+        if (list == null || list.isEmpty()){
             synchronized (lock){
                 list = redisTemplate.opsForList().range("answerQuestions", 0, -1);
-                if (list != null && list.isEmpty()){
-                    init();
+                if (list == null || list.isEmpty()){
+                    list = init();
                 }
             }
         }
